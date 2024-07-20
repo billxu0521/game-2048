1ac9722850a0196585f5b90731c7c5a08022fb81
billxu <billxu0521@gmail.com>
2024-07-21 02:12:45 +0800

    update
    新增走格子
    新增動畫

---
+++ components/SingleRating.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex items-center w-full">
+      <span class="mr-2"></span>
+      <div class="relative w-full max-w-sm h-10 bg-gray-800 rounded-full overflow-hidden">
+        <div class="absolute inset-0 flex space-x-1 p-1">
+          <div
+            v-for="(box,index) in boxes"
+            :key="index"
+            class="w-full h-full bg-green-500"
+          ></div>
+        </div>
+        <div class="absolute inset-0 w-full h-full border-4 border-pink-500 rounded-full"></div>
+      </div>
+    </div>
+  </template>
+
+<script setup lang="ts">
+const boxes = 10;
+</script>
+
+<style scoped>
+.rating-bar {
+    background-color: blue; /* Replace 'blue' with your desired color */
+    height: 10px; /* Adjust the height as needed */
+}
+
+.rating-bar-fill {
+    background-color: yellow; /* Replace 'yellow' with your desired color */
+    height: 100%;
+}
+</style>
---
+++ pages/result/index.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <client-only>
+      <div class="flex flex-col  justify-between items-center h-screen">
+        <div class="flex flex-col items-center">
+          Result header
+        </div>
+        <div class="flex flex-col items-center w-full">
+            Finished
+            
+            
+            <div class="grid grid-cols-2 gap-4 w-full">
+              <div><SingleRating/></div>
+              <div><SingleRating/></div>
+              <div><SingleRating/></div>
+              <div><SingleRating/></div>
+            </div>
+        </div>
+        <div class="flex py-2 px-4 justify-between mb-10">
+          
+        </div>
+        
+      </div>
+    </client-only>
+  </template>
+
+<script setup lang="ts">
+useHead({
+    title: `Result`,
+});
+
+definePageMeta({
+  layout: 'front'
+})
+
+</script>
